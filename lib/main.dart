cff6c08555e9d7c66717d174372bee7d7a457f35
Harun Nurahman <harun.nurrahman@hotmail.com>
2025-03-11 15:08:11 +0700

    Conf: Configurating application, import assets, themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,30 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.green,
+        scaffoldBackgroundColor: Colors.white,
+        colorScheme: ColorScheme.light(
+          primary: Colors.green,
+          secondary: Colors.green.shade400,
+        ),
+        textTheme: GoogleFonts.latoTextTheme(),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            backgroundColor: WidgetStatePropertyAll(Colors.green),
+            shape: WidgetStatePropertyAll(
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            ),
+            padding: WidgetStatePropertyAll(
+              const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+            ),
+            textStyle: WidgetStatePropertyAll(TextStyle(fontSize: 14)),
+          ),
         ),
       ),
+      home: Scaffold(body: Center(child: Text('Hello World!'))),
     );
   }
 }
